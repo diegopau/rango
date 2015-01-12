f3326327abc2895295372f4061c19a64017e69f3
Diego Ocampo <diegoocampo8@gmail.com>
2015-01-12 19:41:25 +0100

    Chapter 6 finished. Learned migrations, database population and admin configuration

--- rango/models.py
+++ rango/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Page(models.Model):
@@ -13,5 +15,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
