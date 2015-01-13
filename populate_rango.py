9f46708fb3d68375ad7f547df2df05742383a289
Diego Ocampo <diegoocampo8@gmail.com>
2015-01-13 22:14:46 +0100

    Chapter 7 finished. Learned more about migrations, added more views and worked with URLs a little bit more.

--- populate_rango.py
+++ populate_rango.py
@@ -13,25 +13,30 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=204)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=56)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=2)
 
     django_cat = add_cat("Django", 32, 64)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=4)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=24)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
@@ -41,11 +46,13 @@ def populate():
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=43)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=1556)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
--- rango/admin.py
+++ rango/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+    list_display = ('title', 'category', 'url', 'views')
 
-admin.site.register(Category)
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ('name', 'slug', 'likes', 'views')
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
-
